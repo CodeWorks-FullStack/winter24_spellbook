a81f93229008aa5317e424493c75a6fca9ecde87
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-14 10:00:22 -0700

    made template, added to model

--- app/models/Spell.js
+++ app/models/Spell.js
@@ -4,6 +4,14 @@ export class Spell {
     this.name = data.name
     this.urlEnding = data.url
   }
+
+  get ListButtonHTMLTemplate() {
+    return `
+    <div class="mb-2">
+      <button class="btn btn-info w-75">${this.name}</button>
+    </div>
+    `
+  }
 }
 
 // const spellData = {
