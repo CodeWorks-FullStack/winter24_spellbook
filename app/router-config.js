517a0141d1dd391a7c6ca2f7f00f4526a14a197e
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-14 09:35:10 -0700

    set up router and html view

--- app/router-config.js
+++ app/router-config.js
@@ -7,15 +7,8 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
   {
     path: '',
-    controllers: [HomeController],
-    view: /*html*/`
-    <div class="bg-white p-3">
-      <div class="card-body">
-        <p>Home Page</p>
-        <button class="btn btn-dark" onclick="app.HomeController.testButton()">😎</button>
-      </div>
-    </div>
-    `
+    controllers: [],
+    view: 'app/views/HomeView.html'
   },
   {
     path: '#/about',
