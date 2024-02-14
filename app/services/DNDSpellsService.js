fd76e5f7d54d893a34e59c017fb366ef57987b5e
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-14 09:56:04 -0700

    mapped over spells, stored in appstate

--- app/services/DNDSpellsService.js
+++ app/services/DNDSpellsService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js";
+import { Spell } from "../models/Spell.js";
 import { dndAPI } from "./AxiosService.js"
 
 class DNDSpellsService {
@@ -5,6 +7,10 @@ class DNDSpellsService {
     const response = await dndAPI.get('api/spells')
 
     console.log('📡 Got spells from DND API', response.data);
+
+    const newSpells = response.data.results.map(spellPOJO => new Spell(spellPOJO))
+
+    AppState.dndSpells = newSpells
   }
 
 }
