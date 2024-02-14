e4e1225b8586df87572401201132138bc4003ae0
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-14 09:49:41 -0700

    got response logging to console

--- app/services/DNDSpellsService.js
+++ app/services/DNDSpellsService.js
@@ -1,6 +1,10 @@
+import { dndAPI } from "./AxiosService.js"
+
 class DNDSpellsService {
   async getDNDSpells() {
-    // const response = await 
+    const response = await dndAPI.get('api/spells')
+
+    console.log('📡 Got spells from DND API', response.data);
   }
 
 }
